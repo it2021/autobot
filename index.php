525e4a2082243e499d2f359e4caf40b6922ed236
itpop2021 <itpop910@gmail.com>
2020-04-18 00:37:14 +0700

    'tdd'

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ $request = file_get_contents('php://input');   // Get request content
 $request_array = json_decode($request, true);   // Decode JSON to Array
 
 
-if (!isset($request_array['events'])) {
+if ( sizeof($request_array['events']) > 0 ) {
 
     foreach ($request_array['events'] as $event) {
 
