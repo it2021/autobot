8357f2fc63fe6db97de2d35e4e46b92bc083d799
itpop2021 <itpop910@gmail.com>
2020-04-17 22:44:56 +0700

    New4

--- index.php
+++ index.php
@@ -1,54 +1,46 @@
-<?php 
-$API_URL = 'https://api.line.me/v2/bot/message';
-$ACCESS_TOKEN = 'qL18gTbZZKqZXBpOOeKHayzoepYsM/t/RY1RxCeBdiW4YCu53VmjLJuwot+2C1bSgSg11D5yGSxHSSp0ABVbVSEnfDYshKZ1Zw8ZWqsROcgNQrwVqVUUCHf+4ciCjK7FPS+hg0ZOu5fBafdYWSwZQgdB04t89/1O/w1cDnyilFU='; 
-$channelSecret = '13cae15b2142e66b213a661e30004dbc';
+<?php // callback.php
+require "./vendor/autoload.php";
+require_once('./vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
+$access_token = 'qL18gTbZZKqZXBpOOeKHayzoepYsM/t/RY1RxCeBdiW4YCu53VmjLJuwot+2C1bSgSg11D5yGSxHSSp0ABVbVSEnfDYshKZ1Zw8ZWqsROcgNQrwVqVUUCHf+4ciCjK7FPS+hg0ZOu5fBafdYWSwZQgdB04t89/1O/w1cDnyilFU=';
 
-
-$POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);
-
-$request = file_get_contents('php://input');   // Get request content
-$request_array = json_decode($request, true);   // Decode JSON to Array
-
-
-
-if ( sizeof($request_array['events']) > 0 ) {
-
-    foreach ($request_array['events'] as $event) {
-
-        $reply_message = '';
-        $reply_token = $event['replyToken'];
-
-        $text = $event['message']['text'];
-        $data = [
-            'replyToken' => $reply_token,
-            // 'messages' => [['type' => 'text', 'text' => json_encode($request_array) ]]  Debug Detail message
-            'messages' => [['type' => 'text', 'text' => $text ]]
-        ];
-        $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
-
-        $send_result = send_reply_message($API_URL.'/reply', $POST_HEADER, $post_body);
-
-        echo "Result: ".$send_result."\r\n";
-    }
+// Get POST body content
+$content = file_get_contents('php://input');
+// Parse JSON
+$events = json_decode($content, true);
+// Validate parsed JSON data
+if (!is_null($events['events'])) {
+// Loop through each event
+foreach ($events['events'] as $event) {
+// Reply only when message sent is in 'text' format
+if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+// Get text sent
+$text = $event['source']['userId'];
+// Get replyToken
+$replyToken = $event['replyToken'];
+// Build message to reply back
+$messages = [
+'type' => 'text',
+'text' => $text
+];
+// Make a POST Request to Messaging API to reply to sender
+$url = 'https://api.line.me/v2/bot/message/reply';
+$data = [
+'replyToken' => $replyToken,
+'messages' => [$messages],
+];
+$post = json_encode($data);
+$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+$result = curl_exec($ch);
+curl_close($ch);
+echo $result . "\r\n";
 }
-
+}
+}
 echo "OK";
-
-
-
-
-function send_reply_message($url, $post_header, $post_body)
-{
-    $ch = curl_init($url);
-    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, $post_header);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $post_body);
-    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-    $result = curl_exec($ch);
-    curl_close($ch);
-
-    return $result;
-}
-
 ?>
