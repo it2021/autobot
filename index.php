b279ab75023568ceee28836382941659da095d74
itpop2021 <itpop910@gmail.com>
2020-04-21 00:18:47 +0700

    notebook_update

--- index.php
+++ index.php
@@ -1,10 +1,7 @@
 <?php
-
-
 $API_URL = 'https://api.line.me/v2/bot/message';
 $ACCESS_TOKEN = 'yTdPdV7qXOJyl5duTlmuSGmSMy/MNqtzLmyewQXR4CbZW31lv3vKaVI+Labr7YbagSg11D5yGSxHSSp0ABVbVSEnfDYshKZ1Zw8ZWqsROchC24rZrizoMHLZx8cYvUYHudCZI74Dne4LoiIJzRNkwQdB04t89/1O/w1cDnyilFU='; 
 $channelSecret = '13cae15b2142e66b213a661e30004dbc';
-
 
 $POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);
 
@@ -21,13 +18,31 @@ if ( sizeof($request_array['events']) > 0 ) {
 
         $texts = $event['message']['text'];
 
-        $text = 'นี่เป็น BOT สำหรับส่งและแจ้งเตือนจดหมาย';
+        $text = 'ผมไม่เข้าใจความหมาย?';
 
         if($texts == 'จดหมาย'){
         	$text = 'คุณต้องการดูจดหมายใช่ไหมครับ';
         }
-
-        
+        if($texts == 'หิว'){
+            $text = 'ทำไมถึงหิวอ่ะ';   
+        }
+        if($texts == 'วันนี้'){
+            $text = 'วันนี้อากาศดีนะ';   
+        }
+        if($texts == 'อกว'){
+    	$file = "https://www.tmd.go.th/xml/weather_report.php?StationNumber=48400";
+	$rss = simplexml_load_file($file);
+	$title = $rss->channel->item->title;
+	$link = $rss->channel->item->link;
+	$cdescription = $rss->channel->item->description;
+	$description = strip_tags($cdescription,"<br>");
+	
+	$string = explode('<br/>',$description);
+	$string = explode(':',$description);
+	$str1 = $string[1];
+	$str_2 = explode(' ', $str1);   
+            $text = 'อุณหภูมิ : '.$str_2[1].' C';
+        }
 
         $data = ['replyToken' => $reply_token,'messages' => [['type' => 'text', 'text' => $text ]]];
 
@@ -38,10 +53,6 @@ if ( sizeof($request_array['events']) > 0 ) {
         echo "Result: ".$send_result."\r\n";
     }
 }
-
-
-
-echo "OK ผ่านครับผม 01 ^^";
 
 
 
