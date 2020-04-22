6cf719519f0dbee6db692c21bd49c7ed1d3988f2
it2021 <43258783+it2021@users.noreply.github.com>
2020-04-22 11:10:52 +0700

    alert 12

--- index.php
+++ index.php
@@ -18,8 +18,18 @@ if($time > '01'){
         $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
 
         $send_result = send_reply_message($API_URL.'/reply', $POST_HEADER, $post_body);
+	
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $post_header);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $post_body);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+    $result = curl_exec($ch);
+    curl_close($ch);
 
-        echo "Result: ".$send_result."\r\n";
+    return $result;
+
 }
 
 if ( sizeof($request_array['events']) > 0 ) {
