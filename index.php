d0387187c0ba26bb421d1ab5185d2a63d6207790
it2021 <43258783+it2021@users.noreply.github.com>
2020-04-22 11:15:13 +0700

    alert end day

--- index.php
+++ index.php
@@ -10,17 +10,6 @@ $request = file_get_contents('php://input');   // Get request content
 $request_array = json_decode($request, true);   // Decode JSON to Array
 $time_now = date('H:i');
 $time = date('i');
-$test = 1;
-if($test == '1'){
-   $text = 'Alert Time'.$time;	
-	
-	$data = ['replyToken' => $reply_token,'messages' => [['type' => 'text', 'text' => $text ]]];
-
-        $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
-
-        $send_result = send_reply_message($API_URL.'/reply', $POST_HEADER, $post_body);
-
-}
 
 if ( sizeof($request_array['events']) > 0 ) {
 
