c561143f55c0090dd424f1a6a04930ea80abbb6a
it2021 <43258783+it2021@users.noreply.github.com>
2020-04-22 11:13:48 +0700

    alert test

--- index.php
+++ index.php
@@ -10,25 +10,15 @@ $request = file_get_contents('php://input');   // Get request content
 $request_array = json_decode($request, true);   // Decode JSON to Array
 $time_now = date('H:i');
 $time = date('i');
-
-if($time > '01'){
+$test = 1;
+if($test == '1'){
    $text = 'Alert Time'.$time;	
+	
 	$data = ['replyToken' => $reply_token,'messages' => [['type' => 'text', 'text' => $text ]]];
 
         $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
 
         $send_result = send_reply_message($API_URL.'/reply', $POST_HEADER, $post_body);
-	
-    $ch = curl_init($url);
-    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, $post_header);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $post_body);
-    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-    $result = curl_exec($ch);
-    curl_close($ch);
-
-    return $result;
 
 }
 
